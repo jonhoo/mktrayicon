22274cf9bbcac499bc4b631d4b3b33b119098bf0
GNUser <goodstuff@dantas.airpost.net>
2019-10-18 12:45:20 -0400

    Update mktrayicon.c

--- mktrayicon.c
+++ mktrayicon.c
@@ -240,7 +240,7 @@ int main(int argc, char **argv)
 
 	/* test if last argument is a pipe */
 	char *command = malloc(1024*sizeof(char));
-	sprintf(command, "test -p %s", argv[argc-1]);
+	sprintf(command, "test -p %s 2>/dev/null", argv[argc-1]);
 	if (system(command) == 0) { /* last argument is a pipe */
 		reader = g_thread_new("watch_fifo", watch_fifo, argv[argc-1]);
 	}
